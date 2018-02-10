fcd560dbdcf90313a821c87a877f07b1df8d0075
LeeReindeer <reindeerlee.work@gmail.com>
2018-02-10 13:33:48 +0800

    Add some about

--- app/src/main/java/xyz/leezoom/cloudmemo/AboutActivity.kt
+++ app/src/main/java/xyz/leezoom/cloudmemo/AboutActivity.kt
@@ -1,9 +1,9 @@
 package xyz.leezoom.cloudmemo
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
-import android.util.Log
 import com.danielstone.materialaboutlibrary.ConvenienceBuilder
 import com.danielstone.materialaboutlibrary.MaterialAboutActivity
 import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem
@@ -12,18 +12,27 @@ import com.danielstone.materialaboutlibrary.model.MaterialAboutCard
 import com.danielstone.materialaboutlibrary.model.MaterialAboutList
 import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.mikepenz.iconics.IconicsDrawable
+import org.jetbrains.anko.share
 import org.jetbrains.anko.toast
 
 class AboutActivity : MaterialAboutActivity() {
 
-  override fun getActivityTitle(): CharSequence? = "About"
+  override fun getActivityTitle(): CharSequence? = getString(R.string.about_title)
 
   override fun getMaterialAboutList(p0: Context): MaterialAboutList {
     val appCardBuilder = MaterialAboutCard.Builder()
     appCardBuilder.addItem(MaterialAboutTitleItem.Builder()
             .text(R.string.app_name)
-            .desc("© 2018 Reindeer Lee")
+            .desc("© 2018 ReindeerLee")
             .icon(R.mipmap.ic_launcher)
+            .setOnClickAction {
+              val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "分享 ${getString(R.string.app_name)}： 分享自@酷安网  https://www.coolapk.com/apk/xyz.leezoom.cloudmemo")
+                type = "text/plain"
+              }
+              startActivity(Intent.createChooser(intent, getString(R.string.about_shar)))
+            }
             .build())
 
     appCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(this,
@@ -31,12 +40,11 @@ class AboutActivity : MaterialAboutActivity() {
                     .icon(CommunityMaterial.Icon.cmd_information_outline)
                     .color(Color.BLACK)
                     .sizeDp(18),
-            "Version",
-            true)
-            .setOnClickAction { toast("check update") })
+            getString(R.string.about_version),
+            false))
 
     appCardBuilder.addItem(MaterialAboutActionItem.Builder()
-            .text("License")
+            .text(getString(R.string.about_license))
             .subText("GPL-3.0")
             .icon(IconicsDrawable(this)
                     .icon(CommunityMaterial.Icon.cmd_book)
@@ -45,8 +53,28 @@ class AboutActivity : MaterialAboutActivity() {
             .setOnClickAction(ConvenienceBuilder.createWebsiteOnClickAction(this, Uri.parse("https://www.gnu.org/licenses/gpl-3.0")))
             .build())
 
+    appCardBuilder.addItem(MaterialAboutActionItem.Builder()
+            .text(getString(R.string.about_hwo_to))
+            .subTextHtml("""<blockquote>
+<p>欢迎使用平行世界的笔记，在这里与其他世界的人类分享你的碎碎念吧。</p>
+</blockquote>
+<strong>说明书：</strong>
+<ol>
+<p>* 你拥有一个其他人无法进入的隐藏世界（世界名是你的用户名）</p>
+<p>* 点击菜单可以添加一个平行的世界</p>
+<p>* 点击 Toolbar 切换世界</p>
+<p>* 笔记状态：</p>
+<p>* 小锁符号 表示这条笔记是否可以编辑。</p>
+<p>* 眼睛符号 表示这个世界的人类是否可以看见这条笔记。</p>
+<p>* 注意：只有笔记的创建者可以修改笔记的状态。</p>
+<p>* Σ(｀L_｀ )</p>
+</ol>
+""")
+            .showIcon(false)
+            .build())
+
     val authorCardBuilder = MaterialAboutCard.Builder()
-    authorCardBuilder.title("Author")
+    authorCardBuilder.title(getString(R.string.about_author))
 
     authorCardBuilder.addItem(MaterialAboutActionItem.Builder()
             .text("Reindeer Lee")
@@ -60,7 +88,7 @@ class AboutActivity : MaterialAboutActivity() {
 
 
     authorCardBuilder.addItem(MaterialAboutActionItem.Builder()
-            .text("View on GitHub")
+            .text(getString(R.string.about_github))
             .icon(IconicsDrawable(this)
                     .icon(CommunityMaterial.Icon.cmd_github_circle)
                     .color(Color.BLACK)
@@ -69,10 +97,10 @@ class AboutActivity : MaterialAboutActivity() {
             .build())
 
     val otherCardBuilder = MaterialAboutCard.Builder()
-    otherCardBuilder.title("Convenience")
+    otherCardBuilder.title(getString(R.string.about_contact))
 
     otherCardBuilder.addItem(MaterialAboutActionItem.Builder()
-            .text("Find me on Telegram")
+            .text(getString(R.string.about_telegram))
             .icon(IconicsDrawable(this)
                     .icon(CommunityMaterial.Icon.cmd_telegram)
                     .color(Color.BLACK)
@@ -85,11 +113,11 @@ class AboutActivity : MaterialAboutActivity() {
                     .icon(CommunityMaterial.Icon.cmd_star)
                     .color(Color.BLACK)
                     .sizeDp(18),
-            "Rate this app",
+            getString(R.string.about_rate),
             null))
 
     val licenseCardBuild = MaterialAboutCard.Builder()
-    licenseCardBuild.title("License")
+    licenseCardBuild.title(getString(R.string.about_license))
 
     initLicenseCard(licenseCardBuild)
 
